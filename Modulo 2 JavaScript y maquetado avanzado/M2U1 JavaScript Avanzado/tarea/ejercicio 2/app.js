df2c8cf9b0950d8e7f20c3b462bfe699c70887f4
Juan Cruz Rodriguez <lacolinajc@gmail.com>
2022-09-08 10:21:03 -0300

    act a m2u1 tarea ejer 2

--- Modulo 2 JavaScript y maquetado avanzado/M2U1 JavaScript Avanzado/tarea/ejercicio 2/app.js
+++ Modulo 2 JavaScript y maquetado avanzado/M2U1 JavaScript Avanzado/tarea/ejercicio 2/app.js
@@ -1,10 +1,28 @@
-/*
-var texto = document.getElementById('texto');
-var contar = document.getElementById('contar');
+/*no entiendo pq esto no funciona si está acá, pero si funciona si esta en el script dentro de html
+var texto = document.getElementById('texto')
+var contar = document.getElementById('contar')
 
 texto.addEventListener('keyup', function(){
-    contar.innerText = texto.value.length;
+    contar.innerText = texto.length
 })
 */
 
-//no entiendo pq esto no funciona si está acá, pero si funciona si esta en el script dentro de html
+let texto = document.getElementById("texto")
+let contar = document.getElementById("contar")
+
+if (texto == null)
+    console.log("abc")
+
+if (texto != null)
+    console.log("abcd")
+
+//como es null la consola no lo lee y tampoco me deja poner addeventlistener
+
+texto.addEventListener("keyup", function(){
+    console.log("abcde")
+})
+
+//tira este error: Uncaught TypeError: Cannot read properties of null (reading 'addEventListener') at app.js:21:7
+//probé con textarea, input text y pasa lo mismo
+//pero si escribo exactamente lo mismo dentro de html funciona
+//intente ponerle un value al input, pero tampoco funciona
