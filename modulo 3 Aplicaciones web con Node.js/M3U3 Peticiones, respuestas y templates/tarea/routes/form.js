a29d3dfa7905832545c47fd0de67b3f2adfd7d68
Juan Cruz Rodriguez <lacolinajc@gmail.com>
2022-10-27 16:46:46 -0300

    M3U3

---
+++ modulo 3 Aplicaciones web con Node.js/M3U3 Peticiones, respuestas y templates/tarea/routes/form.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+const { eventNames } = require('../app');
+
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mendaje;
+
+  var obj = {
+    to: 'lacolinajc@gmail.com',
+    subject: 'contacto desde la web',
+    html: `${nombre} ${apellido} se contacto a traves del formulario en fulvito y quiere mas info a este correo: ${email} ademas, hizo el siguiente comentario: ${mensaje}. su tel es ${tel}`
+  };
+
+  var transporter = nodemailer.createTransport({
+    host: process.eventNames.SMTP_HOST,
+    port: process.eventNames.SMTP_PORT,
+    auth: {
+      user: process.eventNames.SMTP_USER,
+      pass: process.eventNames.SMTP_PASS
+    }
+  });
+
+  var info = await transporter.sendMail(obj);
+
+  res.render('form', {
+    message: 'Mensaje enviado correctamente'
+  });
+});
+
+module.exports = router;
